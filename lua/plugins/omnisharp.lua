3436483c4196c69524b6c45081e01c0bb716cb7e
Jiangzhou Liu <joliu@amergis.com>
2025-05-29 13:54:15 -0400

    Correctly configured omnisharp, renamed vim-tmux-navigator.lua

--- lua/plugins/omnisharp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "Hoffs/omnisharp-extended-lsp.nvim" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ensure_installed = { "c_sharp" }
-    end,
-  },
-}
