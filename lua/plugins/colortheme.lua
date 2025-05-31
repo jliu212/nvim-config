e9a03bf06eab4fd7b7b3526fc767411070841cb8
jliu212 <jiangzhouliu1@gmail.com>
2025-05-31 19:48:43 -0400

    Add catpuccin colortheme

---
+++ lua/plugins/colortheme.lua
@@ -0,0 +1,20 @@
+return {
+  -- -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  --
+  -- -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+  --
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+}
